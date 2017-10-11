efa649a2b094e603844b9c590ffcf4bc800099fd
Tejaswini Ganapathi <tejasvinee@gmail.com>
2017-10-11 01:41:11 -0700

    deleted non needed files

--- python_conversion/initialize_W.py
+++
@@ -1,9 +0,0 @@
-import numpy
-from matlab_functions
-
-def initialize_W(A, delta):
-    dW = numpy.random.randn(A.shape[0], A.shape[1])
-    dW = numpy.dot(delta, normc(dW))
-    W_T = A + dW
-    W= numpy.transpose(W_T)
-    return W, W_T
