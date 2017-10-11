4e62b56a20cacc8e1f354f4056dfdaac90755cc7
Tejaswini Ganapathi <tejasvinee@gmail.com>
2017-10-10 20:23:37 -0700

    re-wrote grad function, please check

--- python_conversion/grad_descent.py
+++ python_conversion/grad_descent.py
@@ -1,34 +1,65 @@
 import numpy
 from matlab_functions import *
+#
+#def grad(W, X, Y, k, delta, epsilon_i):
+#    import ipdb
+#    ipdb.set_trace()
+#    h = X.shape[0]
+#    n = Y.shape[0]
+#    N = Y.shape[1]
+#
+#    grad_mat = numpy.zeros((h,n))
+#    W_T = numpy.transpose(W)
+#
+#    for j in range(0, N):
+#        supp = numpy.sort(numpy.argwhere(X[:,j]!=0), axis = None)
+#
+#        for i in range(0,k):
+#            matrix_factor = numpy.multiply(numpy.dot(numpy.transpose(W_T[:,supp[i]]), Y[:,j]) - epsilon_i, numpy.eye(n)) + numpy.dot(Y[:,j], numpy.transpose(W_T[:, supp[i]]))
+#            
+#            vector_factor = numpy.dot(W_T, numpy.max([0, numpy.dot(W, Y[:,j])], axis = None) ) - Y[:,j]
+#            squared_loss_grad = numpy.dot(matrix_factor, vector_factor)
+#            grad_mat[supp[i],:] = grad_mat[supp[i], :] + numpy.transpose(squared_loss_grad)
+#
+#    grad_mat = 1/N * grad_mat
+#    return grad_mat
 
-def grad(W, X, Y, k, epsilon_i):
-    h = X.shape[0]
-    n = Y.shape[1]
-    N = Y.shape[2]
 
-    grad_mat = numpy.zeros((h,n))
-    W_T = numpy.transpose(W)
+def grad_no_support(W, X, Y, k, delta, epsilon_i):
 
-    for j in range(0, N):
-        supp = numpy.sort(numpy.argwhere(X[:,j]!=0), axis = None)
+    h = W.shape[0]
+    n = Y.shape[0]
+    N = Y.shape[1]
+    grad_mat = numpy.zeros((h, n))
+    for data_index in range(0, N):
+        y = Y[:, data_index]
 
-        for i in range(0,k):
-            matrix_factor = numpy.multiply(numpy.dot(numpy.transpose(W_T[:,supp[i]]), Y[:,j]) - epsilon_i, numpy.eye(n)) + numpy.dot(Y[:,j], numpy.transpose(W_T[:, supp[i]]))
-            vector_factor = numpy.dot(W_T, numpy.max([0, numpy.dot(W, Y[:,j])], axis = None) ) - Y[:,j]
-            squared_loss_grad = numpy.dot(matrix_factor, vector_factor)
-            grad_mat[supp[i],:] = grad_mat[supp[i], :] + numpy.transpose(squared_loss_grad)
+        #computing \Sum_{j=]}^h ReLU(W_j^T - \epsilon_j)W_j - y
+        _sum = 0
+        for j in range(0, h):
+            scalar = numpy.max([0 , numpy.dot(W[j,:], y) - epsilon_i])
+            _sum += scalar*numpy.transpose(W[j,:]) -  y
+
+        for i in range(0, h):
+            scalar_term = 1.0* ((numpy.dot(W[i,:], y) - epsilon_i)>0)
+            square_term = scalar_term * numpy.eye(n) + numpy.dot(y, W[i,:])
+            grad_mat[i, :] += numpy.transpose(numpy.dot(scalar_term * square_term, _sum))
 
     grad_mat = 1/N * grad_mat
     return grad_mat
 
+    
 
-def grad_descent(W_init, X, Y, eta, delta, epsilon_i, threshold, max_iter):
+
+def grad_descent(W_init, X, Y, k, eta, delta, epsilon_i, threshold, max_iter):
+    import ipdb
+    ipdb.set_trace()
     grad_norm = numpy.zeros((max_iter, 1))
     W = W_init
     _iter = 0
 
     while _iter < max_iter:
-        grad_mat = grad(W,X,Y,k, delta, epsilon_i)
+        grad_mat = grad_no_support(W,X,Y,k, delta, epsilon_i)
         grad_norm[_iter] = numpy.linalg.norm(grad_mat, 'fro', None)
         W = W - numpy.dot(eta, grad_mat)
 
@@ -54,3 +85,5 @@ def grad_descent(W_init, X, Y, eta, delta, epsilon_i, threshold, max_iter):
     final_norm = grad_norm(numpy.argwhere(grad_norm > 0))
     final_norm = final_norm[-1]
     W_final = W
+
+    return W_final, final_norm
