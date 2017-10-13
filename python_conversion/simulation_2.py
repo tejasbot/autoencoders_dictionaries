8dfb82e22d39a82a0a39f1911658e1ab0e35fde7
Tejaswini Ganapathi <tejasvinee@gmail.com>
2017-10-13 01:52:11 -0700

    simulation 2

--- python_conversion/simulation_2.py
+++ python_conversion/simulation_2.py
@@ -5,37 +5,42 @@ from initialize import *
 from grad_descent import *
 
 if __name__ == "__main__":
-    H = [1024 2048 4096 8192 8192*2]
-    init_Delta = [2 50]
+    H = [1024, 2048, 4096, 8192, 8192*2]
+    delta_multipliers = [2, 50]
     no_trials = 5
     eta = 0.1
     p = 0.2
     theta = 0.5
     epsilon_i = 1e-4
-    max_iter = 100
-
-    grad_vector = numpy.zeros((len(H), len(init_Delta)+1))
+    max_iter = 500
+    num_datapoints = 1000
+    n = 50
+    
+    import ipdb
+    ipdb.set_trace()
+    
+    grad_vector = numpy.zeros((len(H), len(delta_multipliers)+1))
     
     for index_h, h in enumerate(H):
-        init_delta = init_Delta + [h]
+        dmul = delta_multipliers + [h]
 
         k = int(numpy.ceil(h**p))
         _high = h ** ((1-p)/2 - theta)
         _low = _high/ (h**(p + theta))
-
+        delta = 1/ (h**(2*p + theta))
         
         
-        for index_delta, delta in enumerate(init_delta):
+        for index_delta, d in enumerate(dmul):
             
             
-            for trial in no_trials:
+            for trial in range(0, no_trials):
                 X, Y, X_test, Y_test, A_star, coherence = data_generation(n, h, k, num_datapoints, _low, _high)
-                W, W_T = initialize_W(A_star,h*delta)
-                grad = grad_no_support(W, X, Y, k, delta, epsilon_i)
+                W, W_T = initialize_W(A_star,d*delta)
+                gradient = grad(W, X, Y, k, delta, epsilon_i)
 
-                grad_norm = numpy.linalg.norm(grad, 'fro', None)
+                grad_norm = numpy.linalg.norm(gradient, 'fro', None)
 
-                print "Grad norm for h:{h}, delta: {delta}, trial: {trial} = {grad_norm}".format(h=str(h), delta = str(delta), trial=str(trial))
+                print "Grad norm for h:{h}, delta_multiplier: {d}, trial: {trial} = {grad_norm}".format(h=str(h), delta = str(d), trial=str(trial), grad_norm = grad_norm)
 
                 grad_norm[index_h, index_delta] += grad_norm
 
